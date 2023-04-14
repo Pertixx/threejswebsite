06decdcd32e3fd20f894421b653699b5de25bcd8
AgustinPerticaro <pertix2000@gmail.com>
2023-04-13 23:26:05 -0300

    created components

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
+import React from "react";
+import state from "../store";
+import { useSnapshot } from "valtio";
 
-function CustomButton({type, title, customStyles, handleClick}) {
-
+function CustomButton({ type, title, customStyles, handleClick }) {
+  const snap = useSnapshot(state);
   const generateStyle = (type) => {
     if (type === "filled") {
       return {
-        backgroundColor: '#000',
-        color: '#fff'
-      }
+        backgroundColor: snap.color,
+        color: "#fff",
+      };
     }
-  }
+  };
 
   return (
-    <button className={`px-2 py-1.2 flex-1 rounded-md ${customStyles}`} style={generateStyle(type)}>
+    <button
+      className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+      style={generateStyle(type)}
+      onClick={handleClick}
+    >
       {title}
     </button>
-  )
+  );
 }
 
-export default CustomButton
+export default CustomButton;
