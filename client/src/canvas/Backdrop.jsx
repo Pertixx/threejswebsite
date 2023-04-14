38a2b38135e626a4b95f79280fc09716c9d180a3
AgustinPerticaro <pertix2000@gmail.com>
2023-04-14 00:11:40 -0300

    added shadows to shirt

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -1,7 +1,16 @@
+import { AccumulativeShadows, RandomizedLight } from "@react-three/drei";
+
 import React from "react";
+import { easing } from "maath";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 
 const Backdrop = () => {
-  return <div>Backdrop</div>;
+  return (
+    <AccumulativeShadows position={[0, 0, -0.14]}>
+      <RandomizedLight amount={4} />
+    </AccumulativeShadows>
+  );
 };
 
 export default Backdrop;
