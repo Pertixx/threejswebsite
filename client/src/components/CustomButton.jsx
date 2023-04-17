21fd17999833fcff2c4b6089eacbdb6b1ae8bf4c
AgustinPerticaro <pertix2000@gmail.com>
2023-04-16 21:33:02 -0300

    added the outline button styling

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { getContrastingColor } from "../config/helpers";
 import state from "../store";
 import { useSnapshot } from "valtio";
 
@@ -8,7 +9,13 @@ function CustomButton({ type, title, customStyles, handleClick }) {
     if (type === "filled") {
       return {
         backgroundColor: snap.color,
-        color: "#fff",
+        color: getContrastingColor(snap.color),
+      };
+    } else if (type === "outline") {
+      return {
+        borderWidth: "1px",
+        borderColor: snap.color,
+        color: snap.color,
       };
     }
   };
